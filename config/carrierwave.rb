86e6f973ed093b672dbafe097ea395f14a032543
vagrant <vagrant@precise32.(none)>
2014-02-16 19:47:34 +0000

    3rd try for image, fixing another typo

--- config/carrierwave.rb
+++ config/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV[ 'AWS_ACCESS_KEY'],                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
     
   }
