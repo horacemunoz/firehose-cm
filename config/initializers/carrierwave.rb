9dfee40073c0a7c12d515e3917d49934852f4ef7
vagrant <vagrant@precise32.(none)>
2014-02-16 19:50:50 +0000

    deleting the wrong carrierwave file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV [ 'AWS_ACCESS_KEY'],                        # required
-    :aws_secret_access_key  => ENV ['AWS_SECRET_ACCESS_KEY']                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
     
   }
   config.fog_directory  = ENV['AWS_BUCKET']                     # required
